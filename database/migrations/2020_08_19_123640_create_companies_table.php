495831e7ac51667b165106adec2eaa134753816b
Byron Alfonso <maximuz313@gmail.com>
2020-08-19 12:48:06 +0000

    Add Models and migrations for Company, Country and UserCompany

---
+++ database/migrations/2020_08_19_123640_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->integer('country_id')->unsigned();
+
+            $table
+				->foreign('country_id')
+				->references('id')
+				->on('country');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
